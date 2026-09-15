ca45588f3296aa936069ab4596a57bb62eb75617
DinoEater <160562476+DinoEater-TH@users.noreply.github.com>
2026-09-15 21:59:57 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,42 +1,45 @@
-const CACHE_NAME = 'kku-follow-v3';
+const CACHE_NAME = "kku-follow-v3";
 const urlsToCache = [
-  '/KKU-Follow/',
-  '/KKU-Follow/index.html',
-  '/KKU-Follow/manifest.json',
-  '/KKU-Follow/icons/icon-192.svg',
-  '/KKU-Follow/icons/icon-512.svg'
+  "/KKU-Follow/",
+  "/KKU-Follow/index.html",
+  "/KKU-Follow/manifest.json",
+  "/KKU-Follow/icons/icon-192.svg",
+  "/KKU-Follow/icons/icon-512.svg",
 ];
 
-self.addEventListener('install', event => {
+self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-      .then(() => self.skipWaiting())
+    caches
+      .open(CACHE_NAME)
+      .then((cache) => cache.addAll(urlsToCache))
+      .then(() => self.skipWaiting()),
   );
 });
 
-self.addEventListener('fetch', event => {
+self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
+      return fetch(event.request);
+    }),
   );
 });
 
-self.addEventListener('activate', event => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    }).then(() => self.clients.claim())
+    caches
+      .keys()
+      .then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (cacheName !== CACHE_NAME) {
+              return caches.delete(cacheName);
+            }
+          }),
+        );
+      })
+      .then(() => self.clients.claim()),
   );
 });
